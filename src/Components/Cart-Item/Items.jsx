b04199519446a81aad4b8fc5bc604c64ca8c9cf5
No1RajAryan <55110091+No1RajAryan@users.noreply.github.com>
2021-10-08 17:53:00 +0530

    first commit

--- src/Components/Cart-Item/Items.jsx
+++ src/Components/Cart-Item/Items.jsx
@@ -4,7 +4,7 @@ import useStyle from './style.js'
 function Items() {
     const classes = useStyle()
     return (
-        <div>
+        <div className={classes.root}>
             <h1>i am best</h1>
         </div>
     )
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import {Link} from 'react-router-dom';
-import useStyle from './style.js'
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
+import useStyle from "./style.js";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 function Navbar() {
-    const classes = useStyle()
+  const classes = useStyle();
 
-    return (
-        <div className={classes.root}>
-         <AppBar position="fixed" color="primary">
-           <Toolbar>
-             <Typography component={Link} to="/" variant="h6">
-               Adapat Ready
-             </Typography>
-           </Toolbar>
-         </AppBar> 
-        </div>
-    )
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" color="primary">
+        <Toolbar>
+          <Typography component={Link} to="/" variant="h6">
+            Adapat Ready
+          </Typography>
+          <ShoppingCartIcon />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
