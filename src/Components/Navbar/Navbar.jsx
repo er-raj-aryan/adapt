2ff3117a8d0fda62d8e29e4b4b016fc741419ee1
No1RajAryan <55110091+No1RajAryan@users.noreply.github.com>
2021-10-08 18:19:01 +0530

    first commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,18 +5,26 @@ import Typography from "@mui/material/Typography";
 import { Link } from "react-router-dom";
 import useStyle from "./style.js";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import IconButton from "@mui/material/IconButton";
+import Badge from '@mui/material/Badge';
 
 function Navbar() {
   const classes = useStyle();
 
+  const handleOnClick = () => {};
+
   return (
     <div className={classes.root}>
-      <AppBar position="fixed" color="primary">
-        <Toolbar>
-          <Typography component={Link} to="/" variant="h6">
+      <AppBar position="fixed" color="primary" className={classes.appBar}>
+        <Toolbar className={classes.toolbar}>
+          <Typography component={Link} to="/" variant="h6" className={classes.logo} >
             Adapat Ready
           </Typography>
-          <ShoppingCartIcon />
+          <IconButton component={Link} to="/cart" aria-label="Cart" className={classes.cart} onClick={handleOnClick}>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartIcon />
+            </Badge>
+          </IconButton>
         </Toolbar>
       </AppBar>
     </div>
--- src/Components/Navbar/style.js
+++ src/Components/Navbar/style.js
@@ -2,6 +2,21 @@ import { makeStyles } from "@mui/styles";
 
 const useStyle = makeStyles(() => ({
   root: {},
+  appBar: {
+    backgroundColor: "#e7e3e3 !important",
+  },
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  logo: {
+    textDecoration: "none",
+    color: "#323232bd",
+    fontWeight:"600 !important",
+    fontFamily: "Poppins, sans-serif !important"
+  },
+  cart: {},
 }));
 
-export default useStyle
+export default useStyle;
