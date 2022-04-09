ae377590a26a9e95929b725ddb9d14fe03896545
Raj Aryan <raj@dailycutting.com>
2022-04-09 07:33:12 +0530

    because:remove unused code

--- src/page-not-found/Page-Not-Found.jsx
+++ src/page-not-found/Page-Not-Found.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 import useStyle from "./style";
 
